64afbdaea974c534738be308ecc29bae1967357b
InduniJD <indunijd@gmail.com>
2023-07-14 23:53:47 +0530

    create page and feed page is done

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-          Discover & Share
+          Discover & Share{" "}
           <br className="max-md:hidden" />
           <span className="orange_gradient text-center">
              AI-Powered Prompts
